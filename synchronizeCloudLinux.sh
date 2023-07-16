7784277a5332a38e9b8a661ae336f7dc9a41c1fd
Rahul Madhavan <rahulmadhavan@google.com>
2023-07-16 21:31:27 +0530

    content changes

---
+++ synchronizeCloudLinux.sh
@@ -0,0 +1,25 @@
+# Type the command "cmd < synchronizeCloud.sh"
+# Preliminary Checks:
+# 1. Ensure "gcloud auth login" is already done
+# 2. Ensure that you are on the IISc CISCO VPN
+
+gsutil -m rsync -r assets gs://www.cstheoryseminars.org/assets
+echo "Assets Directory Synced"
+gsutil -m rsync -r content gs://www.cstheoryseminars.org/content
+echo "Contents Directory Synced"
+gsutil -m rsync ./ gs://www.cstheoryseminars.org
+# gsutil cp index.html gs://www.cstheoryseminars.org
+echo "Base Directory Synced"
+
+
+# Now Git changes
+git add -A
+git commit -m "content changes"
+git push
+
+
+# Now attempt to directly update website through SSH
+# Ensure local computer's ssh credentials are added to remote machine's ~/.ssh/authorized_keys 
+ssh -t msrseminar@csacloud.iisc.ac.in -p 3232 "./sync.sh"
+echo "Please check that the website https://www.csa.iisc.ac.in/theoryseminars/ is updated"
+# exit
